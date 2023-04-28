d5ef69d32443782f234b1c762a8f5d8c36a2bf1e
pyzololo <raf.p.2002@gmail.com>
2023-04-28 12:21:57 +0200

    creating a frame of read_data.py script

---
+++ data_reader.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def read_data():
+    
+    # work in progress...
+    
+    return
+
+
+# =============================================================================
+# there will be another script named sth like model_training.py
+# data will be read in the following way:
+#     
+# from data_reader import read_data
+# 
+# data = read_data()
+# =============================================================================
