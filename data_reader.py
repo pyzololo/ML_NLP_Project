0c45296c94f4f1dfaa39d33764a02a4fa5cb7397
pyzololo <raf.p.2002@gmail.com>
2023-05-07 17:29:18 +0200

    function saving data as pd.DataFrame

--- data_reader.py
+++ data_reader.py
@@ -1,17 +1,51 @@
-import pandas as pd
+# import pandas as pd
 
 def read_data():
     
-    # work in progress...
+    import os
+    import re
+    import pandas as pd
+
+    data = []
+    path = '20_newsgroups'
+
+    # Use os.walk() to iterate through directories and files
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            with open(os.path.join(root, file)) as f:
+                lines = f.readlines()
+
+                # Find the index of the first blank line
+                first_blank_line_index = next((i for i, line in enumerate(lines) if line.strip() == ''), None)
+
+                # Extract header content
+                content = ''.join(lines[:first_blank_line_index])
+
+                # Use regular expressions to extract header information
+                From = re.search(r"From: (.*)", content)
+                Subject = re.search(r"Subject: (.*)", content)
+                Organization = re.search(r"Organization: (.*)", content)
+                Lines = re.search(r"Lines: (\d+)", content)
+                Date = re.search(r"Date: (.*)", content)
+
+                # Store the extracted information and the text after the first blank line in a dictionary
+                record = {
+                    'From': From.group(1) if From else '',
+                    'Subject': Subject.group(1) if Subject else '',
+                    'Organization': Organization.group(1) if Organization else '',
+                    'Lines': int(Lines.group(1)) if Lines else 0,
+                    'Date': Date.group(1) if Date else '',
+                    'Text': ''.join(lines[first_blank_line_index + 1:]).strip()
+                }
+
+                # Append the dictionary to the data list
+                data.append(record)
+
+    # Create the DataFrame using the data list
+    df = pd.DataFrame(data, columns=['From', 'Subject', 'Organization', 'Lines', 'Date', 'Text'])
     
-    return
+    return df
 
+#%%
 
-# =============================================================================
-# there will be another script named sth like model_training.py
-# data will be read in the following way:
-#     
-# from data_reader import read_data
-# 
-# data = read_data()
-# =============================================================================
+gigatest = read_data()
